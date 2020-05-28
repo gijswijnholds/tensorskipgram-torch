168d2c249f700c4246c0e3ef3a505d6d74fde6f6
gijswijnholds <gijswijnholds@gmail.com>
2020-05-28 13:34:53 +0200

    finish verb extraction

--- tensorskipgram/data/ukwackypedia.py
+++ tensorskipgram/data/ukwackypedia.py
@@ -8,13 +8,16 @@ from collections import Counter
 
 
 class UKWackypedia(object):
-    def __init__(self, root_folder: str, transform: Optional[Callable] = None, reduce: bool = False):
+    def __init__(self, root_folder: str, transform: Optional[Callable] = None,
+                 reduce: bool = False):
         self.file_list = self.load_file_list(root_folder)
         self.transform = transform
         self.reduce = reduce
+
     def load_file_list(self, folder):
         # folder should be /import/gijs-shared/gijs/corpora/ukwackypedia_split
         return [os.path.join(folder, fn) for fn in os.listdir(folder)]
+
     def parse_file(self, fn: str):
         print("Opening file...")
         all_text = open(fn, 'r', encoding='utf-8').read()
@@ -29,19 +32,12 @@ class UKWackypedia(object):
                 final_sents = [w for s in final_sents for w in s]
         print("Done parsing file!")
         return final_sents
-    def parse_all_files(self, num_workers: int, multi: bool = False):
-        if multi:
-            pool = mp.Pool(num_workers)
-            all_results = pool.map(self.parse_file, self.file_list)
-            all_results_flat = [r for rs in all_results for r in rs]
-            return all_results_flat
-        else:
-            all_results = []
-            num_fn = len(self.file_list)
-            for i, fn in enumerate(self.file_list):
-                print(f'Processing file {i}/{num_fn}...')
-                all_results.append(self.parse_file(fn))
-            return all_results
+
+    def parse_all_files(self, num_workers: int):
+        pool = mp.Pool(num_workers)
+        all_results = pool.map(self.parse_file, self.file_list)
+        all_results_flat = [r for rs in all_results for r in rs]
+        return all_results_flat
 
 
 def parse_sentence_base(s: str):
@@ -66,10 +62,34 @@ def get_verb_args(s: str):
     return vargs
 
 
-my_verb_corpus = UKWackypedia('/import/gijs-shared/gijs/corpora/ukwackypedia_split',
-                              transform=get_verb_args, reduce=True)
-test_fn = my_verb_corpus.file_list[0]
-all_vargs = my_verb_corpus.parse_all_files(multi=True)
-all_vargs_dict = dict(Counter(all_vargs))
-dump_obj_fn(all_vargs_dict,
-            '/import/gijs-shared/gijs/verb_data/verb_counts_all_corpus_verbs.p')
+def merge_verb_triples(all_triples_dict):
+    verb_dict = {}
+    for (v, s, o) in all_triples_dict:
+        if v in verb_dict:
+            if (s, o) in verb_dict[v]:
+                verb_dict[v][(s, o)] += all_triples_dict[(v, s, o)]
+            else:
+                verb_dict[v][(s, o)] = all_triples_dict[(v, s, o)]
+        else:
+            verb_dict[v] = {(s, o): all_triples_dict[(v, s, o)]}
+    return verb_dict
+
+
+if __name__ == '__main__':
+    my_folder = '/import/gijs-shared/gijs'
+    print("Creating corpus reader...")
+    my_verb_corpus = UKWackypedia(os.path.join(my_folder,
+                                               'corpora/ukwackypedia_split'),
+                                  transform=get_verb_args, reduce=True)
+    print("Processing corpus files...")
+    all_vargs = my_verb_corpus.parse_all_files(num_workers=40)
+    print("Processing counts...")
+    all_vargs_dict = dict(Counter(all_vargs))
+    print("Merging counts by verb...")
+    all_verbs_dict = merge_verb_triples(all_vargs_dict)
+    print("Saving verb dict...")
+    dump_obj_fn(all_verbs_dict,
+                os.path.join(my_folder,
+                             'verb_data/verb_counts_all_corpus_verbs_dict.p'))
+    # dump_obj_fn(all_vargs_dict,
+    #             '/import/gijs-shared/gijs/verb_data/verb_counts_all_corpus_verbs.p')

0d58ef85e520b3152e586d46cd5ef7a40a488894
gijswijnholds <gijswijnholds@gmail.com>
2020-06-10 14:41:36 +0200

    Separate embedder from similarity model

--- tensorskipgram/evaluation/model.py
+++ tensorskipgram/evaluation/model.py
@@ -4,43 +4,20 @@ from torch.nn import Embedding, Linear
 from tensorskipgram.evaluation.data import SentenceData
 
 
-class SentenceEmbedderSimilarity(torch.nn.Module):
-    def __init__(self, noun_matrix, subj_verb_cube, obj_verb_cube, hidden_size: int):
-        super(SentenceEmbedderSimilarity, self).__init__()
+class SentenceEmbedder(torch.nn.Module):
+    def __init__(self, noun_matrix, subj_verb_cube, obj_verb_cube, flex_nouns: bool, flex_verbs: bool):
+        super(SentenceEmbedder, self).__init__()
         self.noun_embedding = Embedding.from_pretrained(noun_matrix)
         self.embed_size = self.noun_embedding.embedding_dim
-        self.hidden_size = hidden_size
         self.subj_verb_embedding = Embedding.from_pretrained(subj_verb_cube)
         self.obj_verb_embedding = Embedding.from_pretrained(obj_verb_cube)
-        self.predictor_h = Linear(4*self.embed_size, self.hidden_size)
-        self.predictor_o = Linear(self.hidden_size, 5)
+        if flex_nouns:
+            self.noun_embedding.weight.requires_grad = True
+        if flex_verbs:
+            self.subj_verb_embedding.weight.requires_grad = True
+            self.obj_verb_embedding.weight.requires_grad = True
 
-    def forward(self,
-                X_sentence1: SentenceData,
-                X_sentence2: SentenceData) -> FloatTensor:
-        """Forward a sentence with verbs in it.
-
-        The input will be a pair of the leftover words in a sentence and the
-        verb-args list. For the verb-args list, we apply the matrix for each
-        verb to it's arguments, and add in the vectors of the leftover words.
-        """
-        """
-        :param X_sentence1: [batch_size, num_samples]
-        :param X_sentence2: [batch_size, num_samples]
-        :return: similarity estimation: [batch_size, num_samples]
-        """
-        emb_sentence1 = self.forward_sentence(*X_sentence1)
-        emb_sentence2 = self.forward_sentence(*X_sentence2)
-
-        mult_dist = torch.mul(emb_sentence1, emb_sentence2)
-        abs_dist = torch.abs(torch.add(emb_sentence1, -emb_sentence2))
-        vec_dist = torch.cat((emb_sentence1, emb_sentence2, mult_dist, abs_dist))
-
-        out = torch.nn.Sigmoid()(self.predictor_h(vec_dist))
-        out = torch.nn.LogSoftmax(dim=-1)(self.predictor_o(out))
-        return out
-
-    def embed_verb_args(self, verb_args, arg=None):
+    def embed_verb_args(self, verb_args: LongTensor, arg=None):
         assert arg in ['subj', 'obj', 'transsubj', 'transobj']
         if len(verb_args) == 0:
             verb_mats = torch.tensor([])
@@ -59,18 +36,20 @@ class SentenceEmbedderSimilarity(torch.nn.Module):
             arg_vecs = self.noun_embedding(verb_args[:, 2])
         return verb_mats, arg_vecs
 
-    def forward_sentence(self,
-                         words: LongTensor,
-                         verb_subj: LongTensor,
-                         verb_obj: LongTensor,
-                         verb_trans: LongTensor) -> FloatTensor:
+    def forward(self,
+                words: LongTensor,
+                verb_subj: LongTensor,
+                verb_obj: LongTensor,
+                verb_trans: LongTensor) -> FloatTensor:
         if words.dim() == 2:
             words, verb_subj, verb_obj, verb_trans = words[0], verb_subj[0], verb_obj[0], verb_trans[0]
         word_sum = self.noun_embedding(words).sum(dim=0)
         subj_verb_mats, subj_vecs = self.embed_verb_args(verb_subj, arg='subj')
         obj_verb_mats, obj_vecs = self.embed_verb_args(verb_obj, arg='obj')
-        trans_verb_subj_mats, trans_subj_vecs = self.embed_verb_args(verb_trans, arg='transsubj')
-        trans_verb_obj_mats, trans_obj_vecs = self.embed_verb_args(verb_trans, arg='transobj')
+        trans_verb_subj_mats, trans_subj_vecs = self.embed_verb_args(
+            verb_trans, arg='transsubj')
+        trans_verb_obj_mats, trans_obj_vecs = self.embed_verb_args(
+            verb_trans, arg='transobj')
         verb_mats = torch.cat((subj_verb_mats, obj_verb_mats,
                                trans_verb_subj_mats, trans_verb_obj_mats))
         verb_mats = verb_mats.view(-1, self.embed_size, self.embed_size)
@@ -80,3 +59,51 @@ class SentenceEmbedderSimilarity(torch.nn.Module):
         verb_arg_vecs = torch.bmm(verb_mats, arg_vecs.unsqueeze(-1)).squeeze()
         verb_arg_sum = verb_arg_vecs.sum(dim=0)
         return word_sum + verb_arg_sum
+
+
+class SimilarityLinear(torch.nn.Module):
+    def __init__(self, embed_size: int, hidden_size: int):
+        super(SimilarityLinear, self).__init__()
+        self.embed_size = embed_size
+        self.hidden_size = hidden_size
+        self.predictor_h = Linear(4 * self.embed_size, self.hidden_size)
+        self.predictor_o = Linear(self.hidden_size, 5)
+
+    def forward(self, sent1vec, sent2vec) -> FloatTensor:
+        mult_dist = torch.mul(sent1vec, sent2vec)
+        abs_dist = torch.abs(torch.add(sent1vec, -sent2vec))
+        vec_dist = torch.cat((sent1vec, sent2vec, mult_dist, abs_dist))
+        out = torch.nn.Sigmoid()(self.predictor_h(vec_dist))
+        out = torch.nn.LogSoftmax(dim=-1)(self.predictor_o(out))
+        return out
+
+
+class SentenceEmbedderSimilarity(torch.nn.Module):
+    def __init__(self, noun_matrix, subj_verb_cube, obj_verb_cube,
+                 hidden_size: int, flex_nouns: bool, flex_verbs: bool):
+        super(SentenceEmbedderSimilarity, self).__init__()
+        self.sentence_embedder = SentenceEmbedder(noun_matrix, subj_verb_cube,
+                                                  obj_verb_cube, flex_nouns,
+                                                  flex_verbs)
+        self.embed_size = self.sentence_embedder.noun_embedding.embedding_dim
+        self.hidden_size = hidden_size
+        self.similarity = SimilarityLinear(self.embed_size, self.hidden_size)
+
+    def forward(self,
+                X_sentence1: SentenceData,
+                X_sentence2: SentenceData) -> FloatTensor:
+        """Forward a sentence with verbs in it.
+
+        The input will be a pair of the leftover words in a sentence and the
+        verb-args list. For the verb-args list, we apply the matrix for each
+        verb to it's arguments, and add in the vectors of the leftover words.
+        """
+        """
+        :param X_sentence1: [batch_size, num_samples]
+        :param X_sentence2: [batch_size, num_samples]
+        :return: similarity estimation: [batch_size, num_samples]
+        """
+        emb_sentence1 = self.sentence_embedder(*X_sentence1)
+        emb_sentence2 = self.sentence_embedder(*X_sentence2)
+        output = self.similarity(emb_sentence1, emb_sentence2)
+        return output

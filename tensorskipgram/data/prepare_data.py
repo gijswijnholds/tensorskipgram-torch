5ecd561916a7f2511a7a5d8319ce1864b4143708
gijswijnholds <gijswijnholds@gmail.com>
2020-11-11 17:27:02 +0100

    remove redundant fns

--- tensorskipgram/data/prepare_data.py
+++ tensorskipgram/data/prepare_data.py
@@ -1,6 +1,5 @@
 import os
-from tensorskipgram.data.preprocessing import Preprocessor
-from tensorskipgram.data.training_data_creator import DataCreator
+from tensorskipgram.data.training_data_creator import Preprocessor, DataCreator
 from tensorskipgram.data.dataset import MatrixSkipgramDataset
 from torch.utils.data import Dataset, DataLoader
 
@@ -11,11 +10,5 @@ verb_dict_fn = os.path.join(folder, 'verb_data/verb_counts_all_corpus_verbs_dict
 verbs_fn = os.path.join(folder, 'verb_data/sick_verbs_full.txt')
 preproc_fn = os.path.join(folder, 'verb_data/preproc_sick_verbcounts.p')
 
-
-# my_preproc = Preprocessor(preproc_fn, space_fn, verb_dict_fn, verbs_fn)
-# my_data_creator = DataCreator(my_preproc, data_folder)
-
-subj_data_fn = os.path.join(data_folder, 'subj_train_data.p')
-obj_data_fn = os.path.join(data_folder, 'obj_train_data.p')
 # subj_dataset = MatrixSkipgramDataset(subj_data_fn, arg='subject', negk=5)
 # obj_dataset = MatrixSkipgramDataset(obj_data_fn, arg='object', negk=5)

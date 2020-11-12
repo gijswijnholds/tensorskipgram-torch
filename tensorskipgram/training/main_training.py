9605b10c84c572e8cae41ba0601f7802e78d3e67
gijswijnholds <gijswijnholds@gmail.com>
2020-11-12 17:51:20 +0100

    Rewrite training loop

--- tensorskipgram/training/main_training.py
+++ tensorskipgram/training/main_training.py
@@ -1,108 +1,64 @@
-from tensorskipgram.config import preproc_fn, svo_triples_fn, verblist_fn, noun_space_fn
-from tensorskipgram.config import model_path_subj, model_path_obj
+import torch
+from tensorskipgram.preprocessing import Preprocessor
+from tensorskipgram.config \
+    import preproc_fn, svo_triples_fn, verblist_fn, noun_space_fn
+from tensorskipgram.config \
+    import model_path_subj, model_path_obj, subj_data_fn, obj_data_fn
+from tensorskipgram.training.model import MatrixSkipgram
+from tensorskipgram.training.dataset import MatrixSkipgramDataset
+from tensorskipgram.training.trainer import train_epoch
+from torch.utils.data import DataLoader
 
 
-def main_old(bs=1, lr=0.005, ne=5):
-    my_preproc = Preprocessor(preproc_fn, noun_space_fn, svo_triples_fn, verblist_fn)
-    subj_i2w = my_preproc.preproc['subj']['i2w']
-    obj_i2w = my_preproc.preproc['obj']['i2w']
-    verb_i2v = my_preproc.preproc['verb']['i2v']
-    lower2upper = my_preproc.preproc['l2u']
-
-    # noun_vocab_size = len(subj_i2w)
-    # context_vocab_size = len(obj_i2w)
-    noun_vocab_size = len(obj_i2w)
-    context_vocab_size = len(subj_i2w)
+def prepare_model(arg: str, context: str, space_fn: str):
+    """Prepare a matrix-skipgram model with a preprocessor and a noun space."""
+    assert arg in ['subj', 'obj']
+    assert context in ['subj', 'obj']
+    assert arg != context
+    preprocessor = Preprocessor(preproc_fn, space_fn, svo_triples_fn,
+                                verblist_fn).preproc
+    arg_i2w, context_i2w = preprocessor[arg]['i2w'], preprocessor[context]['i2w']
+    verb_i2v, lower2upper = preprocessor['verb']['i2v'], preprocessor['l2u']
+    noun_vocab_size, context_vocab_size = len(arg_i2w), len(context_i2w)
     functor_vocab_size = len(verb_i2v)
-    noun_matrix = create_noun_matrix(noun_space_fn, obj_i2w, lower2upper)
-    # noun_matrix = create_noun_matrix(subj_i2w, lower2upper)
-    noun_matrix = torch.tensor(noun_matrix, dtype=torch.float32)
+    noun_matrix_np = create_noun_matrix(space_fn, obj_i2w, lower2upper)
+    noun_matrix = torch.tensor(noun_matrix_np, dtype=torch.float32)
+    return MatrixSkipgram(noun_vocab_size=noun_vocab_size,
+                          functor_vocab_size=functor_vocab_size,
+                          context_vocab_size=context_vocab_size,
+                          embed_size=100, noun_matrix=noun_matrix)
 
 
-    # print("Preparing data loader...")
-    # subj_dataset = MatrixSkipgramDataset(subj_data_fn, arg='subject', negk=5)
-    # subj_dataloader6 = DataLoader(subj_dataset, shuffle=True, batch_size=1)
+def train_model(arg: str, context: str, space_fn: str, bs: int, lr: float):
+    print("Preparing model...")
+    model = prepare_model(arg, context, space_fn)
+
     print("Preparing data loader...")
-    subj_dataset = MatrixSkipgramDataset(subj_data_fn, arg='subject', negk=5)
-    subj_dataloader = DataLoader(subj_dataset, shuffle=True, batch_size=bs)
-    # print("Preparing data loader...")
-    # obj_dataset = MatrixSkipgramDataset(obj_data_fn, arg='object', negk=5)
-    # obj_dataloader = DataLoader(obj_dataset, shuffle=True, batch_size=bs)
+    if arg == 'subj':
+        dataset = MatrixSkipgramDataset(subj_data_fn, arg='subject', negk=5)
+        dataloader = DataLoader(dataset, shuffle=True, batch_size=bs)
+    elif arg == 'obj':
+        dataset = MatrixSkipgramDataset(obj_data_fn, arg='object', negk=5)
+        dataloader = DataLoader(dataset, shuffle=True, batch_size=bs)
 
-    print("Training model...")
+    model.to(1)
+    optimiser = torch.optim.Adam(model.parameters(), lr=lr)
+    loss_fn = torch.nn.BCEWithLogitsLoss()
 
-    subj_matskipgram_modelGPU = MatrixSkipgram(noun_vocab_size=noun_vocab_size,
-                                              functor_vocab_size=functor_vocab_size,
-                                              context_vocab_size=context_vocab_size,
-                                              embed_size=100, noun_matrix=noun_matrix)
-
-    # obj_matskipgram_modelGPU.to('cuda')
-    subj_matskipgram_modelGPU.to(1)
-    optGPU = torch.optim.Adam(subj_matskipgram_modelGPU.parameters(), lr=lr)
-    loss_fnGPU = torch.nn.BCEWithLogitsLoss()
-
-    NUM_EPOCHS = ne
+    NUM_EPOCHS = 1
     epoch_losses = []
     for i in range(NUM_EPOCHS):
-        epoch_loss = train_epoch(subj_matskipgram_modelGPU, subj_dataloader,
-                                 loss_fnGPU, optGPU, device=1, epoch_idx=i+1)
-                                 # loss_fnGPU, optGPU, device='cuda', epoch_idx=i+1)
-        # epoch_loss = train_epoch(subj_matskipgram_modelGPU, obj_dataloader6,
-                                 # loss_fnGPU, optGPU, device='cuda', epoch_idx=i+1)
+        epoch_loss = train_epoch(model, dataloader,
+                                 loss_fn, optimiser, device=1, epoch_idx=i+1)
         epoch_losses.append(epoch_loss)
         print("Saving model...")
         e = i+1
-        torch.save(subj_matskipgram_modelGPU, model_path_subj+f'_bs={bs}_lr={lr}_epoch{e}.p')
+        torch.save(model, model_path_subj+f'_bs={bs}_lr={lr}_epoch{e}.p')
         print("Done saving model, ready for another epoch!")
 
     return epoch_losses
 
-def loadArgAnalysers():
-    subj_preprocFileName = '/import/gijs-shared/gijs/skipprob_data/preproc_sick_subj.pkl'
-    obj_preprocFileName = '/import/gijs-shared/gijs/skipprob_data/preproc_sick_obj.pkl'
-    print("Loading preprocs...")
-    subj_preproc = load_obj_fn(subj_preprocFileName)
-    obj_preproc = load_obj_fn(obj_preprocFileName)
-
-    subj_i2w, subj_w2i, subj_i2c, subj_i2ns = (subj_preproc['index2word'],
-                                               subj_preproc['word2index'],
-                                               subj_preproc['index2count'],
-                                               subj_preproc['index2negsample'])
-
-    obj_i2w, obj_w2i, obj_i2c, obj_i2ns = (obj_preproc['index2word'],
-                                           obj_preproc['word2index'],
-                                           obj_preproc['index2count'],
-                                           obj_preproc['index2negsample'])
-    return (subj_i2w, subj_w2i, subj_i2c, subj_i2ns,
-            obj_i2w, obj_w2i, obj_i2c, obj_i2ns)
-            
-# subjDataset = MatrixSkipgramDataset(sick_subj_data_fn_subj, arg='subject', negk=5)
-# objDataset = MatrixSkipgramDataset(sick_obj_data_fn_obj, arg='object')
-
-# subj_dataloader6 = DataLoader(subjDataset,
-#                               shuffle=True,
-#                               batch_size=1)
-#
-# subj_dataloader48 = DataLoader(subjDataset,
-#                                shuffle=True,
-#                                batch_size=8)
-#
-# subj_dataloader96 = DataLoader(subjDataset,
-#                                shuffle=True,
-#                                batch_size=16)
-#
-# subj_dataloader96 = DataLoader(subjDataset,
-#                                shuffle=True,
-#                                batch_size=64)
-# print("Loading analysers...")
-# (subj_i2w, subj_w2i, subj_i2c, subj_i2ns,
-#  obj_i2w, obj_w2i, obj_i2c, obj_i2ns) = \
-#     loadArgAnalysers()
-
-# noun_vocab_size = len(obj_i2w)
-# context_vocab_size = len(subj_i2w)
-# nounMatrix = createNounMatrix(obj_i2w)
-# nounMatrix = torch.tensor(nounMatrix, dtype=torch.float32)
 
 def main():
-  pass
+    train_model('subj', 'obj', noun_space_fn, 11, 0.001)
+    train_model('obj', 'subj', noun_space_fn, 11, 0.001)
--- tensorskipgram/training/model.py
+++ tensorskipgram/training/model.py
@@ -4,13 +4,16 @@ from torch.nn import Embedding
 
 
 class Skipgram(torch.nn.Module):
-    def __init__(self, vocab_size: int, context_vocab_size: int, embed_size: int):
+    def __init__(self, vocab_size: int, context_vocab_size: int,
+                 embed_size: int):
         super(Skipgram, self).__init__()
         self.target_embedding = Embedding(vocab_size, embed_size)
         self.context_embedding = Embedding(context_vocab_size, embed_size)
 
     def forward(self, X_target: Tensor, X_context: Tensor) -> FloatTensor:
         """
+        Forward pass of the network.
+
         :param X_target: [batch_size]
         :param X_context: [batch_size]
         :return:
@@ -30,24 +33,30 @@ class MatrixSkipgram(torch.nn.Module):
         assert noun_matrix.shape[1] == embed_size
         self.embed_size = embed_size
         self.argument_embedding = Embedding.from_pretrained(noun_matrix)
-        self.functor_embedding = Embedding(functor_vocab_size, embed_size * embed_size)
+        self.functor_embedding = Embedding(functor_vocab_size,
+                                           embed_size * embed_size)
         self.context_embedding = Embedding(context_vocab_size, embed_size)
 
     def forward(self, X_argument: Tensor,
                 X_functor: Tensor,
                 X_context: Tensor) -> FloatTensor:
         """
+        Forward pass of the network.
+
         :param X_argument: [batch_size, num_samples, embed_size]
         :param X_functor: [batch_size, num_samples, embed_size*embed_size]
         :param X_context: [batch_size, num_samples, embed_size]
         :return: batch_dot: [batch_size, num_samples]
         """
         batch_size, num_samples = X_argument.shape
-        arg_vecs = self.argument_embedding(X_argument).view(-1, self.embed_size)
+        arg_vecs = self.argument_embedding(X_argument).view(-1,
+                                                            self.embed_size)
         func_mats = self.functor_embedding(X_functor).view(-1, self.embed_size,
                                                            self.embed_size)
-        context_vecs = self.context_embedding(X_context).view(-1, self.embed_size)
+        context_vecs = self.context_embedding(X_context).view(-1,
+                                                              self.embed_size)
 
         funcarg_vecs = torch.bmm(func_mats, arg_vecs.unsqueeze(-1)).squeeze()
-        batch_dot = torch.sum(funcarg_vecs * context_vecs, dim=1).view(batch_size, num_samples)
+        batch_dot = torch.sum(funcarg_vecs * context_vecs,
+                              dim=1).view(batch_size, num_samples)
         return batch_dot

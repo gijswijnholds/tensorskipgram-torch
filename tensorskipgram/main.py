1d89d8268dd99a3085db74d26a9ba8995693a5a5
gijswijnholds <gijswijnholds@gmail.com>
2020-12-08 16:50:58 +0100

    Fix imports

--- tensorskipgram/main.py
+++ tensorskipgram/main.py
@@ -4,13 +4,13 @@ import numpy as np
 from tqdm import tqdm
 from torch.utils.data import DataLoader
 from tensorskipgram.trainer import train_epoch
-from tensorskipgram.data.preprocessing import Preprocessor
+from tensorskipgram.preprocessing.training_data_creator import Preprocessor
 from tensorskipgram.config import subj_data_fn, obj_data_fn, noun_space_fn
 from tensorskipgram.data.dataset import create_noun_matrix, MatrixSkipgramDataset
 from tensorskipgram.models.model import MatrixSkipgram
-from tensorskipgram.data.main_preprocessing import main as preprocessing_main
-from tensorskipgram.data.main_evaluation import main as evaluation_main
-from tensorskipgram.data.main_training import main as training_main
+from tensorskipgram.preprocessing.main_preprocessing import main as preprocessing_main
+from tensorskipgram.evaluation.main_evaluation import main as evaluation_main
+from tensorskipgram.training.main_training import main as training_main
 
 
 def final_main():
